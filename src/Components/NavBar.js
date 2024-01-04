e6f6c6c34e6c011deaa77d4753b33c05a10272af
utsav82 <utsa.yadav.94@gmail.com>
2024-01-04 20:19:16 +0530

    updated navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,40 +1,84 @@
-import React from "react"
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import img from '../res/onlyaagaz.png'
-import { Link } from 'react-router-dom'
-import '../Pages/style/navbar.css'
+import React, { useState } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import img from "../res/onlyaagaz.png";
+import { Link } from "react-router-dom";
+import "../Pages/style/navbar.css";
 
 const NavBar = () => {
-    return (
-        <Navbar collapseOnSelect style={{ 'width': "100vw", 'color': "rgb(255,255,255)", }} variant="dark" expand="lg">
-            <Container>
-                <Navbar.Brand style={{ 'font-size': "20px", 'letterSpacing': "2px" }} align-items-center href="/">
-                    <img
-                        alt="img"
-                        src={img}
-                        width="30"
-                        height="25"
-                        className="d-inline-block align-top"
-                    />{' '}
-                    AAGAZ'23</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav style={{ 'font-size': "10px" }} className="navLinks ms-auto">
-                        <Link className='navLinks1' to="/">Home</Link>&nbsp;
-                        <Link className='navLinks1' to="/events0">Events</Link>&nbsp;
-                        <Link className='navLinks1' to="/sponsers">Sponsors</Link>&nbsp;
-                        <Link className='navLinks1' to="/merch">Merch and Passes</Link>&nbsp;
-                        <Link className='navLinks1' to="/gallery">Gallery</Link>&nbsp;
-                        {/* <Link className='navLinks1' to="/subCouncils">Sub-councils</Link> */}
-                        &nbsp;&nbsp;
-                        {/* <Button variant="danger" size="sm">
-                            <a style={{ textDecoration: 'none', fontSize: '12px', fontWeight: '100', color: 'white' }} href='https://docs.google.com/forms/d/e/1FAIpQLSfgCZTeX1Ss5tUbY108CoSbXZh6VvUkuFnRhGt-2SXqJYnDNA/viewform?usp=sf_link' target='_blank' rel="noopener noreferrer">Be Our Campus Ambassador!</a>
-                        </Button> */}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
-}
+  const [isNavbarOpen, setIsNavbarOpen] = useState(true);
+  const handleNavLinkClick = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+  return (
+    <Navbar
+      collapseOnSelect
+      style={{ width: "80vw", color: "rgb(255,255,255)" }}
+      variant="dark"
+      expand="lg"
+      expanded={isNavbarOpen}
+    >
+      <Container>
+        <Navbar.Brand
+          style={{ "font-size": "20px", letterSpacing: "2px" }}
+          align-items-center
+          href="/"
+        >
+          <img
+            alt="img"
+            src={img}
+            width="30"
+            height="25"
+            className="d-inline-block align-top"
+          />{" "}
+          AAGAZ'23
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={handleNavLinkClick}
+        />
+        <Navbar.Collapse className="responsive-navbar-nav">
+          <Nav style={{ "font-size": "10px" }} className="navLinks ms-auto">
+            <Link className="navLinks1" to="/" onClick={handleNavLinkClick}>
+              Home
+            </Link>
+            &nbsp;
+            <Link
+              className="navLinks1"
+              to="/events0"
+              onClick={handleNavLinkClick}
+            >
+              Events
+            </Link>
+            &nbsp;
+            <Link
+              className="navLinks1"
+              to="/sponsers"
+              onClick={handleNavLinkClick}
+            >
+              Sponsors
+            </Link>
+            &nbsp;
+            <Link
+              className="navLinks1"
+              to="/merch"
+              onClick={handleNavLinkClick}
+            >
+              Merch and Passes
+            </Link>
+            &nbsp;
+            <Link
+              className="navLinks1"
+              to="/gallery"
+              onClick={handleNavLinkClick}
+            >
+              Gallery
+            </Link>
+            &nbsp;
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,57 +1,96 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Image from "react-bootstrap/Image";
-import Button from 'react-bootstrap/Button';
-import '../App.css';
+import Button from "react-bootstrap/Button";
+import "../App.css";
 
 import back from "../res/firstbgaagaz.png";
 import adhyaay_logo from "../res/aagaztext.png";
 import adhyaay_logo_whitered from "../res/onlyaagaz.png";
 
 const home = () => {
-    const homeStyle = {
-        height: '100vh',
-        backgroundImage: `url(${back})`,
-        fontSize: '50px',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-    };
-    const aboutStyle = {
-        height: '500px',
-        width: '100%',
-        backgroundColor: '',
-        backgroundImage:
-            "url('https://dhruvil-lakhtaria.github.io/incident-2022/assets/img/xl.svg'), url('https://dhruvil-lakhtaria.github.io/incident-2022/assets/img/sm.svg')",
-    };
-    return (
-        <section>
-            <center>
-                <div style={homeStyle} className="d-flex align-items-center justify-content-center landingPage">
-                    <Image overflow="hidden" src={adhyaay_logo} rounded className="mainPageAdhyaayLogo" />
-                    <Button variant="outline-danger" size="lg">
-                        <a style={{ textDecoration: 'none', fontWeight: '600', color: 'white' }} href='https://forms.gle/Fh3XgEZ2ShbcnwCt9' target='_blank' rel="noopener noreferrer">Register Now</a>
-                    </Button>
-                </div>
-                <div style={aboutStyle}>
-                    <div className='aboutStyleGrid'>
-                        <div className='aboutStyleGridText'>
-                            <h1>About</h1>
-                            <br></br>
-                            <p>AAGAZ'23 is the descendant of the program initiated by Sports Sub-Council. The event aims to bring all the sports events under the auspices of a Mega Inter-College sports fest!!
-                                <br></br>
-                                <br />
-                                AAGAZ'23 will be a new & better version of AAGAZ'22 which was a successful event. This will be an invaluable opportunity for brands to advertise themselves in front of the innovative lads.
-                            </p>
-                        </div>
-                        <div className='aboutStyleGridPic'>
-                            <Image overflow="hidden" src={adhyaay_logo_whitered} rounded style={{ height: "200px", width: "230px", }} className="adhyaayLogo" />
-                        </div>
-                    </div>
-                </div>
-            </center>
-            {/* <Footer /> */}
-        </section>
-    )
-}
+  const homeStyle = {
+    height: "100vh",
+    backgroundImage: `url(${back})`,
+    fontSize: "50px",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
+  const aboutStyle = {
+    height: "500px",
+    width: "100%",
+    backgroundColor: "",
+    backgroundImage:
+      "url('https://dhruvil-lakhtaria.github.io/incident-2022/assets/img/xl.svg'), url('https://dhruvil-lakhtaria.github.io/incident-2022/assets/img/sm.svg')",
+  };
+  return (
+    <section>
+      <center>
+        <div
+          style={homeStyle}
+          className="flex flex-col items-center justify-center"
+        >
+          <Image
+            overflow="hidden"
+            src={adhyaay_logo}
+            rounded
+            className="mainPageAdhyaayLogo"
+          />
+          <Button
+            variant="danger"
+            size="lg"
+            style={{
+              fontWeight: "600",
+              borderRadius: "10px",
+            }}
+          >
+            <a
+              style={{
+                textDecoration: "none",
+                fontWeight: "600",
+                color: "white",
+                display: "block",
+                padding: "5px",
+              }}
+              href="https://forms.gle/Fh3XgEZ2ShbcnwCt9"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Register Now
+            </a>
+          </Button>
+        </div>
+        <div style={aboutStyle}>
+          <div className="aboutStyleGrid">
+            <div className="aboutStyleGridText">
+              <h1>About</h1>
+              <br></br>
+              <p>
+                AAGAZ'23 is the descendant of the program initiated by Sports
+                Sub-Council. The event aims to bring all the sports events under
+                the auspices of a Mega Inter-College sports fest!!
+                <br></br>
+                <br />
+                AAGAZ'23 will be a new & better version of AAGAZ'22 which was a
+                successful event. This will be an invaluable opportunity for
+                brands to advertise themselves in front of the innovative lads.
+              </p>
+            </div>
+            <div className="aboutStyleGridPic">
+              <Image
+                overflow="hidden"
+                src={adhyaay_logo_whitered}
+                rounded
+                style={{ height: "200px", width: "230px" }}
+                className="adhyaayLogo"
+              />
+            </div>
+          </div>
+        </div>
+      </center>
+      {/* <Footer /> */}
+    </section>
+  );
+};
 
-export default home
+export default home;
